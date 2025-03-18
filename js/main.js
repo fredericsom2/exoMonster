43355edb10bc48727d1abe427a7adef9beb16de8
fredericsom2 <frederic.som@lapiscine.pro>
2025-03-18 16:22:03 +0100

    initialisation de mon projet

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 document.getElementById('hamburger').addEventListener('click', function() {
     this.classList.toggle('active');
 });
+
+// Ajouter des événements aux menus
+document.getElementById('menu1').addEventListener('click', function() {
+    alert('Menu 1 cliqué!');
+});
+
+document.getElementById('menu2').addEventListener('click', function() {
+    alert('Menu 2 cliqué!');
+});
+
+document.getElementById('menu3').addEventListener('click', function() {
+    alert('Menu 3 cliqué!');
+});
+
+document.getElementById('menu4').addEventListener('click', function() {
+    alert('Menu 4 cliqué!');
+});
